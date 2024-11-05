104e891fd11ec7b9eb157986bb25904bba2113d7
Foshati <amirrezafoshati@gmail.com>
2024-11-05 23:47:04 +0330

    render html file

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.http import  HttpResponse
+from django.shortcuts import render
 
 
 def sayHello(request):
-    return HttpResponse("hello for all")
+    return render(request, "home.html")
